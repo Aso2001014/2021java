3c1b9f3e174e6a759c517156f49d784ec3bff3d3
Aso2001014 <83051321+Aso2001014@users.noreply.github.com>
2021-07-30 20:36:16 +0900

    Delete jk01-05.java

--- jk01-05.java
+++
@@ -1,8 +0,0 @@
-public class JSSJK0105{
-    public static void main(String[] args){
-        int num = Integer.parseInt(args[0]);
-           if (num == 0){
-		   System.out.println("num is 0");
-		   }
-    }
-}
