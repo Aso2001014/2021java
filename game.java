8a21572baffb8c91aca9810ec1b5399fd228b060
Aso2001014 <83051321+Aso2001014@users.noreply.github.com>
2021-07-30 20:36:06 +0900

    Create game.java

---
+++ game.java
@@ -0,0 +1,30 @@
+package java;
+import java.util.Scanner;
+import java.util.Random;
+public class game {
+    Scanner sr = new Scanner(System.out.print(in);
+    Romdom rand = new Random()();
+
+    Ship ship1 = new Ship();
+    Ship ship2= new Ship();
+    Ship ship3 = new Ship();
+
+    int cnt =1;
+
+    public void game(){
+        while (Ship.checkShip()){
+            System.out.println();
+
+            System.out.println(1);
+            System.out.println(2);
+            System.out.println(3);
+
+            System.out.println("爆弾のx座標を入力してください");
+            int choose_coorX=sc.nextInt();
+            System.out.println("爆弾のx座標を入力してください");
+            int choose_coorX=sc.nextInt();
+        
+        }
+    }
+    
+}
