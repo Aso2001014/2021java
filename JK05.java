0d78298d87e533a82ef07bab56f1cd70c1b73643
Aso2001014 <83051321+Aso2001014@users.noreply.github.com>
2021-07-30 20:42:19 +0900

    Add files via upload

---
+++ JK05.java
@@ -0,0 +1,11 @@
+public class JK05{
+    public static void main(String[] args){
+	int num = 10;
+	int add =1;
+	for(int i =0; i < 9; i++){
+		add = add*num;
+		num--;
+		}
+		System.out.println(add);
+	}
+}
